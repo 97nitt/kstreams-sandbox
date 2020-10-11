a4b11628b725d7e53b119c9b0c454f44a828fe85
Corey Fritz <corey.fritz@gmail.com>
2020-10-11 08:54:52 -0400

    Added print statements to help verify messages in output streams

--- src/main/java/sandbox/kafka/streams/zmart/ZMartApp.java
+++ src/main/java/sandbox/kafka/streams/zmart/ZMartApp.java
@@ -6,6 +6,7 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.Produced;
 import sandbox.kafka.streams.KafkaStreamsApplication;
 import sandbox.kafka.streams.serde.JsonSerde;
@@ -35,14 +36,17 @@ public class ZMartApp extends KafkaStreamsApplication {
     // create stream of purchases, with masked credit card numbers
     KStream<String, Purchase> purchases = builder.stream("transactions", Consumed.with(stringSerde, purchaseSerde))
         .mapValues(p -> Purchase.builder(p).maskCreditCard().build());
+    purchases.print(Printed.<String, Purchase>toSysOut().withLabel("purchases"));
     purchases.to("purchases", Produced.with(stringSerde, purchaseSerde));
 
     // create stream of purchase patterns
     KStream<String, PurchasePattern> patterns = purchases.mapValues(p -> PurchasePattern.builder(p).build());
+    patterns.print(Printed.<String, PurchasePattern>toSysOut().withLabel("patterns"));
     patterns.to("patterns", Produced.with(stringSerde, purchasePatternSerde));
 
     // create stream of reward data
     KStream<String, Rewards> rewards = purchases.mapValues(p -> Rewards.builder(p).build());
+    rewards.print(Printed.<String, Rewards>toSysOut().withLabel("rewards"));
     rewards.to("rewards", Produced.with(stringSerde, rewardsSerde));
 
     return builder.build();
